ed39f447ffb78b53ab18f9369f93c4cbf68a8f8e
Akanksha Vyas <vyasakanksha@gmail.com>
2020-08-05 16:07:53 +0530

    cleaned up and added code to create a new book for BookCollection WIP

--- bookshop/functions/index.js
+++ bookshop/functions/index.js
@@ -3,94 +3,38 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp()
 const path = require('path');
-const os = require('os');
-const neatCsv = require('neat-csv');
 const csvtojson = require("csvtojson")
-var books = require('google-books-search');
-const {google} = require('googleapis');
 const fs = require('fs');
-var array = require('lodash/array');
 const db = admin.firestore();
+var book = require('./Book')
+
+
 db.settings({ ignoreUndefinedProperties: true })
 
 // const {Translate} = require('@google-cloud/translate').v2;
 
 const runtimeOpts = {
-    timeoutSeconds: 300,
+    timeoutSeconds: 540,
     memory: '128MB'
   }
 
-// [END import]
-
-// [START generateThumbnail]
-/**
- * When an image is uploaded in the Storage bucket We generate a thumbnail automatically using
- * ImageMagick.
- */
-// exports.uploadBooks = functions.storage.object().onFinalize(async (object) => {
-exports.uploadBooks = functions.runWith(runtimeOpts).https.onRequest(async (req, res) => {
-//   // [START eventAttributes]
-//   const fileBucket = object.bucket; // The Storage bucket that contains the file.
-//   const filePath = object.name; // File path in the bucket.
-//   const contentType = object.contentType; // File content type.
-//   const metageneration = object.metageneration; // Number of times metadata has been generated. New objects have a value of 1.
-//   // [END eventAttributes]
-
-//   // [START stopConditions]
-//   // Exit if this is triggered on a file that is not an image.
-//   if (!contentType.startsWith('image/') || !contentType.startsWith('text/csv')) {
-//     return console.log('This is not an image or spreadsheet.');
-//   }
+async function asyncForEach(array, callback) {
+    for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+    }
+}
 
 const extractFields = [
     'ISBN', 
+    'Title',
+    'Author',
+    'Publisher',
     'Date Added',
-    'Uploaded Image URL',
+    'Image URL',
     'Category',
-    'List Price'
+    'List Price',
+    'Source'
 ];
-
-parsedData = []
-data = []
-const dir = './rawfilesBookBuddy'
-const files = fs.readdirSync(dir)
-
-for (file of files) {
-    var tempData = await csvtojson().fromFile(path.join(dir, file));
-    data.push(...tempData)
-    for(let i of tempData) {
-        var temp = {}
-        extractFields.map(nm => {
-            temp[nm] = i[nm];
-        })
-        parsedData.push(temp)
-    }
-}
-parsedData = array.uniqBy(parsedData, 'Date Added')
-
-for(let i of data) {
-    addtocollection("rawData", i, i['Date Added'])
-}
-
-const booksapi = google.books({
-    version: 'v1',
-    auth: "AIzaSyBSbWl6-e2C7eNJZ_p-JmStcTTKaPrhY00", // specify your API key here
-});
-
-// const translate = new Translate();
-
-// async function translateText(text, target) {
-//     // Translates the text into the target language. "text" can be a string for
-//     // translating a single piece of text, or an array of strings for translating
-//     // multiple texts.
-//     let [translations] = await translate.translate(text, target);
-//     translations = Array.isArray(translations) ? translations : [translations];
-//     console.log('Translations:');
-//     translations.forEach((translation, i) => {
-//       console.log(`${text[i]} => (${target}) ${translation}`);
-//     });
-//   }
-
 
 function addtocollection(collection, item, key) {
     var docRef = db.collection(collection).doc(key);
@@ -113,64 +57,35 @@ function addtocollection(collection, item, key) {
     });
 }
 
-remaining = []
-collection = []
-for(let i of parsedData.slice(0, 80)) {
-    console.log(i['ISBN'])
-    const gresp = await booksapi.volumes.list({q: i['ISBN'], printType: 'books', langRestrict: 'hi'});
+exports.uploadBooks = functions.runWith(runtimeOpts).https.onRequest(async (req, res) => {
+    parsedData = []
+    data = []
+    temp = {}
 
-    if (!gresp.data.items) {
-        console.log("REMAINING")
-        data.filter(function (_,n){
-            if (data[n]['ISBN'] === i['ISBN']) {
-                remaining.push(data[n])
-            }
-        });
-    } else {
-        let book = {
-            isbn: i['ISBN'],
-            title: gresp.data.items[0].volumeInfo.title, 
-            author: gresp.data.items[0].volumeInfo.authors, 
-            publisher: gresp.data.items[0].volumeInfo.publisher,
-            genre: "",
-            distributor: "",
-            price: i['List Price'].replace(/\D/g,''),
-            image_url: "",
-            category: i['Category']
-         };
+    const dir = './rawFiles'
+    const files = fs.readdirSync(dir)
 
-         let stock = {
-            book: book,
-            count: 1,
-         };
-         addtocollection("books", book, i['ISBN'])
-         addtocollection("inStock", stock, i['ISBN'])
-        //  console.log(translateText(gresp.data.items[0].volumeInfo.title, 'hi'))
+    async function readfiles(files, dir) {
+        data = []
+        for (file of files) {
+            console.log(file)
+            var tempData = await csvtojson().fromFile(path.join(dir, file));
+            data.push(...tempData)
+        }
     }
-}
+    await readfiles(files, dir)
+    for (let d in data) {
+        if (data[d]['Source'] == 'bookbuddy') {
+            console.log((data[d]['Image URL']))
+        }
+        parsedData.push(parseData(data[d], extractFields))
+    }
 
-//   // [START thumbnailGeneration]
-//   // Download file from bucket.
-//   const bucket = admin.storage().bucket(fileBucket);
-//   const tempFilePath = path.join(os.tmpdir(), fileName);
-//   const metadata = {
-//     contentType: contentType,
-//   };
-//   await bucket.file(filePath).download({destination: tempFilePath});
-//   console.log('Image downloaded locally to', tempFilePath);
-//   // Generate a thumbnail using ImageMagick.
-//   await spawn('convert', [tempFilePath, '-thumbnail', '200x200>', tempFilePath]);
-//   console.log('Thumbnail created at', tempFilePath);
-//   // We add a 'thumb_' prefix to thumbnails file name. That's where we'll upload the thumbnail.
-//   const thumbFileName = `thumb_${fileName}`;
-//   const thumbFilePath = path.join(path.dirname(filePath), thumbFileName);
-//   // Uploading the thumbnail.
-//   await bucket.upload(tempFilePath, {
-//     destination: thumbFilePath,
-//     metadata: metadata,
-//   });
-//   // Once the thumbnail has been uploaded delete the local file to free up disk space.
-//   return fs.unlinkSync(tempFilePath);
-//   // [END thumbnailGeneration]
+    function parseData(data, extractFields) {
+        extractFields.map(nm => {
+            temp[nm] = data[nm];
+        })
+        fields = Object.values(temp)
+        b = new book.Book(fields[0], fields[1], fields[2], fields[3], fields[4], fields[5], fields[6], fields[7], fields[8])
+    }
 });
-// [END generateThumbnail]
