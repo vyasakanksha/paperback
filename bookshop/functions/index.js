c033c000d362a29f18f327de532d9ca9e9691160
Akanksha Vyas <vyasakanksha@gmail.com>
2020-08-09 14:25:19 +0530

    cloud function to craete thumbnail from images for book covers

--- bookshop/functions/index.js
+++ bookshop/functions/index.js
@@ -7,6 +7,9 @@ const csvtojson = require("csvtojson")
 const fs = require('fs');
 const db = admin.firestore();
 var book = require('./Book')
+const mkdirp = require('mkdirp');
+const spawn = require('child-process-promise').spawn;
+const os = require('os');
 
 
 db.settings({ ignoreUndefinedProperties: true })
@@ -88,4 +91,80 @@ exports.uploadBooks = functions.runWith(runtimeOpts).https.onRequest(async (req,
         fields = Object.values(temp)
         b = new book.Book(fields[0], fields[1], fields[2], fields[3], fields[4], fields[5], fields[6], fields[7], fields[8])
     }
+});
+
+// Max height and width of the thumbnail in pixels.
+const THUMB_MAX_HEIGHT = 160;
+const THUMB_MAX_WIDTH = 107;
+// Thumbnail prefix added to file names.
+const THUMB_PREFIX = 'thumb_';
+
+/**
+ * When an image is uploaded in the Storage bucket We generate a thumbnail automatically using
+ * ImageMagick.
+ * After the thumbnail has been generated and uploaded to Cloud Storage,
+ * we write the public URL to the Firebase Realtime Database.
+ */
+exports.generateThumbnail = functions.storage.object().onFinalize(async (object) => {
+    // File and directory paths.
+    const filePath = object.name;
+    const contentType = object.contentType; // This is the image MIME type
+    const fileDir = path.dirname(filePath);
+    const fileName = path.basename(filePath);
+    const thumbFilePath = path.normalize(path.join(fileDir, `${THUMB_PREFIX}${fileName}`));
+    const tempLocalFile = path.join(os.tmpdir(), filePath);
+    const tempLocalDir = path.dirname(tempLocalFile);
+    const tempLocalThumbFile = path.join(os.tmpdir(), thumbFilePath);
+
+    // Exit if this is triggered on a file that is not an image.
+    if (!contentType.startsWith('image/')) {
+    return console.log('This is not an image.');
+    }
+
+    // Exit if the image is already a thumbnail.
+    if (fileName.startsWith(THUMB_PREFIX)) {
+    return console.log('Already a Thumbnail.');
+    }
+
+    // Cloud Storage files.
+    const bucket = admin.storage().bucket(object.bucket);
+    const file = bucket.file(filePath);
+    const thumbFile = bucket.file(thumbFilePath);
+    const metadata = {
+    contentType: contentType,
+    // To enable Client-side caching you can set the Cache-Control headers here. Uncomment below.
+    // 'Cache-Control': 'public,max-age=3600',
+    };
+
+    // Create the temp directory where the storage file will be downloaded.
+    await mkdirp(tempLocalDir)
+    // Download file from bucket.
+    await file.download({destination: tempLocalFile});
+    console.log('The file has been downloaded to', tempLocalFile);
+    // Generate a thumbnail using ImageMagick.
+    await spawn('convert', [tempLocalFile, '-thumbnail', `${THUMB_MAX_WIDTH}x${THUMB_MAX_HEIGHT}>`, tempLocalThumbFile], {capture: ['stdout', 'stderr']});
+    console.log('Thumbnail created at', tempLocalThumbFile);
+    // Uploading the Thumbnail.
+    await bucket.upload(tempLocalThumbFile, {destination: thumbFilePath, metadata: metadata});
+    console.log('Thumbnail uploaded to Storage at', thumbFilePath);
+    // Once the image has been uploaded delete the local files to free up disk space.
+    fs.unlinkSync(tempLocalFile);
+    fs.unlinkSync(tempLocalThumbFile);
+    // Get the Signed URLs for the thumbnail and original image.
+    const config = {
+        action: 'read',
+        expires: '03-01-2500',
+    };
+    const results = await Promise.all([
+    thumbFile.getSignedUrl(config),
+    file.getSignedUrl(config),
+    ]);
+    console.log('Got Signed URLs.');
+    const thumbResult = results[0];
+    const originalResult = results[1];
+    const thumbFileUrl = thumbResult[0];
+    const fileUrl = originalResult[0];
+    // Add the URLs to the Database
+    // await admin.database().ref('images').push({path: fileUrl, thumbnail: thumbFileUrl});
+    return console.log('Thumbnail URLs saved to database.', thumbFileUrl);
 });
